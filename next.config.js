5624a1dfab75c91b3549f3e3e658c8f9370d532d
JackpotProtocol <niyoo999@gmail.com>
2025-11-26 01:29:16 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer, buildId, dev }) => {
-    // 解决 pino-pretty 模块找不到的问题
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'pino-pretty': false,
-    }
-    
-    // 可选：也忽略 pino 的其他问题依赖
+  // 添加这行配置来解决问题
+  webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      'worker_threads': false,
       'pino-pretty': false,
     }
-    
     return config
   },
 }
